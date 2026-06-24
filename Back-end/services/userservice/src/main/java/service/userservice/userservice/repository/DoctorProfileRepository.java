7b908754b9d68333c7a839716944e805d8e74a68
Taposh Kumer Ghosh <gtaposh88@gmail.com>
2026-06-24 19:03:44 +0600

    adding query

--- Back-end/services/userservice/src/main/java/service/userservice/userservice/repository/DoctorProfileRepository.java
+++ Back-end/services/userservice/src/main/java/service/userservice/userservice/repository/DoctorProfileRepository.java
@@ -3,6 +3,7 @@ package service.userservice.userservice.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,9 +14,10 @@ public interface DoctorProfileRepository extends JpaRepository<DoctorProfile, St
     List<DoctorProfile> findByApprovalTrue();
     long countByApprovalTrue();
 
-    /*@Query("SELECT d FROM DoctorProfile d WHERE d.approval = true "
+    @Query("SELECT d FROM DoctorProfile d WHERE d.approval = true "
             + "AND (:specialization IS NULL OR LOWER(d.specialization) LIKE LOWER(CONCAT('%', :specialization, '%'))) "
-            + "AND (:location IS NULL OR LOWER(d.location) LIKE LOWER(CONCAT('%', :location, '%')))")*/
+            + "AND (:location IS NULL OR LOWER(d.location) LIKE LOWER(CONCAT('%', :location, '%')))")
+
     List<DoctorProfile> searchApprovedDoctors(
             @Param("specialization") String specialization,
             @Param("location") String location);
